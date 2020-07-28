8a9351b300c41cf614da3e815885317e3b12a3d3
Rakesh Kumawat <rk23597@gmail.com>
2020-07-28 10:06:13 +0530

    added bootstrap

--- MyAddressBookPlus/Global.asax.cs
+++ MyAddressBookPlus/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Azure.KeyVault;
-using System.Web.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -11,10 +13,6 @@ namespace MyAddressBookPlus
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-
-            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(KeyVaultService.GetToken));
-            var sec = kv.GetSecretAsync(WebConfigurationManager.AppSettings["CacheConnectionSecretUri"]).Result;
-            KeyVaultService.CacheConnection = sec.Value;
         }
     }
 }
